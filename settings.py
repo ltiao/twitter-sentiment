251371fe268e409aa37d1564672ada835a697cbb
U-NEXUS\tia00c <tia00c@PARMA-EP.tip.csiro.au>
2013-12-16 10:33:59 +1100

    added settings

---
+++ settings.py
@@ -0,0 +1,41 @@
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s [%(levelname)s] (%(threadName)-10s): %(message)s',
+            'datefmt': '%m/%d/%Y %I:%M:%S %p'
+        },
+        'simple': {
+            'format': '%(asctime)s %(levelname)s %(message)s',
+            'datefmt': '%m/%d/%Y %I:%M:%S %p'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level':'INFO',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': 'twitter_MAC.log',
+            'maxBytes': 1024 * 1024, # 1 mb,
+            'backupCount': 15,
+            'formatter': 'simple',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'twitter': {
+            'handlers': ['default', 'console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
