4b6d0f7cbed62818e890df44466057175c3688fa
Louis Tiao <louistiao@gmail.com>
2014-01-03 17:23:28 +1100

    got dynamic test generation working

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import regex
 from htmlentitydefs import name2codepoint
-import re
 
-HTML_ENTITY_REGEX = re.compile('&#?(?P<entity>\d+|{0});'.format('|'.join(name2codepoint)))
+HTML_ENTITY_REGEX = regex.compile('&#?(?P<entity>\d+|{0});'.format('|'.join(name2codepoint)))
 
-URL_REGEX = re.compile(
+URL_REGEX = regex.compile(
     r"""[a-z][a-z0-9+\-.]*://                                       # Scheme
     ([a-z0-9\-._~%!$&'()*+,;=]+@)?                                  # User
     (?P<host>[a-z0-9\-._~%]+                                        # Named Host
@@ -16,88 +16,89 @@ URL_REGEX = re.compile(
     (/[a-z0-9\-._~%!$&'()*+,;=:@]+[a-z0-9+&@#/%=~_|$])*/?           # Path
     (\?[a-z0-9\-._~%!$&'()*+,;=:@/?]*[a-z0-9+&@#/%=~_|$])?          # Query
     (\#[a-z0-9\-._~%!$&'()*+,;=:@/?]*[a-z0-9+&@#/%=~_|$])?          # Fragment
-    """, re.IGNORECASE | re.VERBOSE)
+    """, regex.IGNORECASE | regex.VERBOSE)
 
-MENTION_REGEX = re.compile(
+MENTION_REGEX = regex.compile(
     ur"""
-    (?:(?<![a-zA-Z0-9_!#\$%&*@＠])|(?<=^)|(?<=RT:)|(?<=RT))
+    (?<=[^a-zA-Z0-9_!#\$%&*@＠]|^|RT:?)
     [@＠]
     [a-zA-Z0-9_]{1,20}
     (\/[a-zA-Z][a-zA-Z0-9_\-]{0,24})?
-    """, re.UNICODE | re.VERBOSE)
+    """, regex.UNICODE | regex.VERBOSE | regex.IGNORECASE)
 
-REPEATED_CHAR_REGEX = re.compile(r'(\w)\1{2,}')
+REPEATED_CHAR_REGEX = regex.compile(r'(\w)\1{2,}')
 
-class Processor(object):
+
+class TwitterTextPreprocessor(object):
     
     def __init__(self):
         pass
 
-def decode_html_entities(s):
+    def __call__(self, s):
+        return self.preprocess(s)
 
-    def repl_func(m):
-        entity = m.group('entity') # get the entity name or number
-        try:
-            # if integer
-            codepoint = int(entity)       
-        except ValueError: 
-            # not integer - it must be named and therefore 
-            # in name2codepoint (i.e. codepoint is never None)
-            codepoint = name2codepoint.get(entity)
-        # if codepoint > 16**2, or for some other 
-        # reason we cannot encode, just leave as-is
-        try:
-            return unichr(codepoint)
-        except ValueError:
-            return m.group()
-            
-    return HTML_ENTITY_REGEX.sub(repl_func, s)
+    def preprocess(self, s):
+        func_list = (self.decode_html_entities, self.normalize_urls, self.normalize_mentions, self.normalize_repeated_chars)
+        return reduce(lambda x, y: y(x), func_list, s)
 
-def normalize_urls(s, incl_domain=False):
-    
-    def repl_func(m):
-        return '[URL-{0}]'.format(m.group('host'))
+    def decode_html_entities(self, s):
 
-    if incl_domain:
-        repl = repl_func
-    else:
-        repl = '[URL]'
-    return URL_REGEX.sub(repl, s)
+        def repl_func(m):
+            entity = m.group('entity') # get the entity name or number
+            try:
+                # if integer
+                codepoint = int(entity)       
+            except ValueError: 
+                # not integer - it must be named and therefore 
+                # in name2codepoint (i.e. codepoint is never None)
+                codepoint = name2codepoint.get(entity)
+            # if codepoint > 16**2, or for some other 
+            # reason we cannot encode, just leave as-is
+            try:
+                return unichr(codepoint)
+            except ValueError:
+                return m.group()
+                
+        return HTML_ENTITY_REGEX.sub(repl_func, s)
 
-def normalize_repeated_chars(s):
-    return REPEATED_CHAR_REGEX.sub(r'\1\1\1', s)
+    def normalize_urls(self, s, incl_domain=False):
+        
+        def repl_func(m):
+            return '[URL-{0}]'.format(m.group('host'))
 
-def normalize_mentions(s):
-    
-    def repl_func(m):
-        return '[@MENTION]'
-    
-    return MENTION_REGEX.sub(repl_func, s)
+        if incl_domain:
+            repl = repl_func
+        else:
+            repl = '[URL]'
+        return URL_REGEX.sub(repl, s)
+
+    def normalize_mentions(self, s, repl_func='@MENTION'):
+        return MENTION_REGEX.sub(repl_func, s)
+
+    def normalize_repeated_chars(self, s):
+        return REPEATED_CHAR_REGEX.sub(r'\1\1\1', s)
 
 if __name__ == '__main__':
-    test = u"Check @_@ this ＠test sieee f@@k sh@#t eete @tiao http://www.google.com out http://t.co/FNkPfmii- it's great lolololol hahahahahah"
 
-    print test
-    print normalize_urls(test)
-    print normalize_repeated_chars(test)
-    print normalize_mentions(test)
-    
+    print reduce(lambda x, y: '{0}({1})'.format(y, x), ['f', 'g', 'h'], 'x')
+
+    preprocessor = TwitterTextPreprocessor()
+
     from pymongo import MongoClient
 
     client = MongoClient()
 
     db = client.twitter_database
     db_labeled_tweets = db.labeled_tweets
-    
+
     for tweet in db_labeled_tweets.find({u'text': {'$exists': True}}):
         text = tweet.get(u'text')
+        print tweet.get(u'_id')
         print text
-        print decode_html_entities(text)
-        print normalize_urls(text)
-        print normalize_repeated_chars(text)
-        print normalize_mentions(text)
-        print normalize_urls(normalize_repeated_chars(normalize_mentions(decode_html_entities(text) )))
+        # print decode_html_entities(text)
+        # print normalize_urls(text)
+        # print normalize_repeated_chars(text)
+        # print normalize_mentions(text)
+        # print normalize_urls(normalize_repeated_chars(normalize_mentions(decode_html_entities(text))))
+        print preprocessor(text).encode('utf-8')
         print
-        
-    m = re.search('(?<=abc)def', 'abcdef def defdef ab')
-    print m.group()
--- tests.py
+++ tests.py
@@ -1,56 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import random
 import unittest
+import random
+import yaml
+import re
 
-from tokenize import TwitterTokenizer
+from preprocess import TwitterTextPreprocessor
 
-class TestSequenceFunctions(unittest.TestCase):
 
-    def setUp(self):
-        self.seq = range(10)
-
-    def test_shuffle(self):
-        # make sure the shuffled sequence does not lose any elements
-        random.shuffle(self.seq)
-        self.seq.sort()
-        self.assertEqual(self.seq, range(10))
-
-        # should raise an exception for an immutable sequence
-        self.assertRaises(TypeError, random.shuffle, (1,2,3))
-
-    def test_choice(self):
-        element = random.choice(self.seq)
-        self.assertTrue(element in self.seq)
-
-    def test_sample(self):
-        with self.assertRaises(ValueError):
-            random.sample(self.seq, 20)
-        for element in random.sample(self.seq, 5):
-            self.assertTrue(element in self.seq)
-
-class TestTwitterTokenizer(unittest.TestCase):
+class TestTwitterPreprocessor(unittest.TestCase):
     
     def setUp(self):
-        self.twokenizer = TwitterTokenizer()
+        with open('twitter-text-conformance/extract.yml', 'r') as extractor_tests_infile:
+            self.extractor_tests = yaml.load(extractor_tests_infile)
+        self.preprocessor = TwitterTextPreprocessor() 
 
-    # TODO: Add everything from http://en.wikipedia.org/wiki/List_of_emoticons (including Eastern emoticons)
-    def test_emoticons(self):
-        strings = (
-            u':-) :) :o) :] :3 :c) :> =] 8) =) :} :^) :っ)',
-            u':-D :D 8-D 8D x-D xD X-D XD =-D =D =-3 =3', # omitted: B^D
-            u'>:[ :-( :( :-c :c :-< :っC :< :-[ :[ :{',
-        )
-        for string in strings:
-            tokens = self.twokenizer.tokenize(string)
-            self.assertEqual(string, ' '.join(tokens))
+    # TODO: Dynamically generate test method instead of making assertions in a loop in one test method,
+    # using nosetests or some other method described in:
+    # http://stackoverflow.com/questions/32899/how-to-generate-dynamic-parametrized-unit-tests-in-python
+    def test_mentions(self):
+        for test in self.extractor_tests['tests']['mentions']:
+            text = test['text']
+            if test['expected']:
+                expected_text = re.sub('|'.join(re.escape(exp) for exp in test['expected']), 'MENTION', text)
+            else:
+                expected_text = text
+            print self.preprocessor.normalize_mentions(text)
+            print expected_text
+            print
+            self.assertEqual(self.preprocessor.normalize_mentions(text), expected_text)
 
-    def test_hashtag(self):
-        string = u'@soem83thing @test'
-        tokens = self.twokenizer.tokenize(string)
-        self.assertEqual(string, ' '.join(tokens))
-
+class KnownGood(unittest.TestCase):
+    def __init__(self, test):
+        super(KnownGood, self).__init__()
+        self.text = test['text']
+        if test['expected']:
+            self.expected_text = re.sub('|'.join(re.escape(exp) for exp in test['expected']), 'MENTION', self.text)
+        else:
+            self.expected_text = self.text
+        self.preprocessor = TwitterTextPreprocessor() 
+        
+    def runTest(self):
+        self.assertEqual(self.preprocessor(self.text), self.expected_text)
+        
+def suite():
+    suite = unittest.TestSuite()
+    with open('twitter-text-conformance/extract.yml', 'r') as extractor_tests_infile:
+        extractor_tests = yaml.load(extractor_tests_infile)
+    suite.addTests(KnownGood(test) for test in extractor_tests['tests']['mentions'])
+    return suite
 
 if __name__ == '__main__':
-    unittest.main()
+    #unittest.main()
+    unittest.TextTestRunner().run(suite())
